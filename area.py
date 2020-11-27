4fe555461a9fcdf45a281fe49f7ea5c2fb9dd66a
nyi13 <nyilinhtut1994@gmail.com>
2020-11-27 22:38:29 +0630

    version5

--- area.py
+++ area.py
@@ -2,3 +2,10 @@ height = input("Enter height in m ===> ")
 width = input("Enter width in m===> ")
 area = int(height) * int(width)
 print("Area ===>" , area, "meters")
+
+#in linux scripts
+#echo -n "Enter height in m ===> "
+#read height
+#echo -n "Enter width in m ===> "
+#read width
+#echo "area is `expr $height * $width` meter"
