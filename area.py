715572623120a00fe2e368d8d21bd6092d1fa922
nyi13 <nyilinhtut1994@gmail.com>
2020-11-27 22:13:19 +0630

    version4

--- area.py
+++ area.py
@@ -1,4 +1,4 @@
 height = input("Enter height in m ===> ")
 width = input("Enter width in m===> ")
 area = int(height) * int(width)
-print("Area ===>" , area, "meter")
+print("Area ===>" , area, "meters")
